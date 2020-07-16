90c87281e825f0fcbda0aff396fe623e7f5a5f05
Neo Chan <netkiller@msn.com>
2020-07-16 10:55:54 +0800

    DB

--- mac.sh
+++ mac.sh
@@ -1,4 +1,6 @@
+git submodule update
 #rm -rf ~/tmp/architect/
 mkdir -p ~/tmp/architect/
 cp -r images ~/tmp/architect/
+cp common/docbook.css ~/tmp/management/
 xsltproc -o ~/tmp/architect/ docbook-xsl/docbook.mac.xsl book.xml
