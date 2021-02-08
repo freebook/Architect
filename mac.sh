f2a99ca3a165f74f7caf2bba843df2083382b23c
Neo Chan <netkiller@msn.com>
2021-02-08 17:41:05 +0800

    网络延迟

--- mac.sh
+++ mac.sh
@@ -5,4 +5,4 @@ mkdir -p ~/tmp/architect/
 cp -r images ~/tmp/architect/
 #cp common/docbook.css ~/tmp/architect/docbook.css
 cp common/docbook-xsl.css ~/tmp/architect/docbook.css
-xsltproc -o ~/tmp/architect/ docbook-xsl/docbook.mac.xsl book.xml
+xsltproc -o ~/tmp/architect/ docbook-xsl/docbook.preview.xsl book.xml
